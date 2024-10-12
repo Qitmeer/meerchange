11680858d2b0f7e444ca3fa366981073b900d611
Jin <lochjin@gmail.com>
2024-10-12 22:21:24 +0800

    solidity 0.8.26

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import { HardhatUserConfig } from 'hardhat/config'
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{
-      version: '0.8.15',
+      version: '0.8.26',
       settings: {
         optimizer: { enabled: true, runs: 1000000 }
       }
